c5d296d6a9aef811c84a206b7971ee1c0f330f4a
Exe Developer <exedevcoding22@gmail.com>
2023-07-17 20:09:54 -0300

    add page catalogo and filters

--- frontend/src/pages/catalogo/[filtro].js
+++ frontend/src/pages/catalogo/[filtro].js
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
 import React from 'react'
 
-const PageFiltro = () => {
+const PageFiltro = ({}) => {
   return (
     <Layout>
       
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import { Testimonials } from "@/components/Testimonials";
 import { CardSlider } from "@/components/CardSlider";
 import Footer from "@/components/shared/footer/Footer";
-import Content from "@/components/contact/ContentContact";
+import ContentContact from "@/components/contact/ContentContact";
 import Carrousel from "@/components/carrousel/Carrousel";
 import NavBar from "@/components/NavBar/NavBar";
 
@@ -10,32 +10,34 @@ const inter = Inter({ subsets: ["latin"] });
 const image1 = "/carrousel/carrousel1.svg";
 const image2 = "/carrousel/carrousel1.svg";
 const image3 = "/carrousel/carrousel1.svg";
-export default function Home({data}) {
 
-  console.log(data);
+
+export default function Home() {
+
+  
   return (
     <>
-      {/* <NavBar /> */}
+      <NavBar />
       <Carrousel image1={image1} image2={image2} image3={image3} />
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
       >
         <CardSlider type="offers" />
         <CardSlider type="tendencies" />
-        {/* <Testimonials /> */}
+        <Testimonials />
       </main>
-      <Content />
+      <ContentContact />
       <Footer />
     </>
   );
 }
 
-export const getServerSideProps = async () => {
-  const response = await fetch(`${process.env.DOMAIN}/products`);
-  const data = await response.json();
-  return {
-    props: {
-      data,
-    },
-  };
-}
+// export const getServerSideProps = async () => {
+//   const response = await fetch(`${process.env.DOMAIN}/products`);
+//   const data = await response.json();
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
