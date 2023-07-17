9f6b9ae2e17144be7727bcc74d7cdfab38d8fd73
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-17 18:54:36 -0400

    fix: fetch product data with getServerSideProps

--- frontend/src/components/CarDetails/RelatedCars.jsx
+++ frontend/src/components/CarDetails/RelatedCars.jsx
@@ -1,14 +1,13 @@
-import { useRelatedProducts } from '@/hooks/useRelatedProducts';
 import { Card } from '../Card';
 
-export const RelatedCars = ({ brand, id }) => {
-    const { data } = useRelatedProducts(brand, id);
+export const RelatedCars = ({ data, id }) => {
+    const relatedCars = data.filter((item) => item.id !== id).slice(0, 3);
 
     return (
         <div className="flex flex-col items-center bg-Neutral pt-9 pb-20 gap-8">
             <h2 className="text-SlateGray font-bold text-3xl">Autos relacionados</h2>
             <div className="flex gap-5 md:flex-row flex-col">
-                {data.map((item) => (
+                {relatedCars.map((item) => (
                     <Card key={item.id} data={item} />
                 ))}
             </div>
