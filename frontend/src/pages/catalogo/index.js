93015595f58849f9d2f6d43370036c6829816168
Exe Developer <exedevcoding22@gmail.com>
2023-07-18 10:44:37 -0300

    page catalogo finish

--- frontend/src/pages/catalogo/index.js
+++ frontend/src/pages/catalogo/index.js
@@ -1,3 +1,4 @@
+import { Card } from "@/components/Card";
 import Filtered from "@/components/Filterss/Filtered";
 import Layout from "@/components/Layout";
 import ContentContact from "@/components/contact/ContentContact";
@@ -7,8 +8,7 @@ const Catalogo = ({ products }) => {
   const [dataFiltered, setDataFiltered] = useState("");
   const [resFilter, setResFilter] = useState([]);
 
-  const cardsCatalogue = products.slice(0, 12)
-  console.log(cardsCatalogue);
+  const dataCatalogue = products.slice(0, 12);
 
   useEffect(() => {
     const getDataFilter = async (filter) => {
@@ -23,7 +23,7 @@ const Catalogo = ({ products }) => {
 
   return (
     <Layout page="Catálogo">
-      <header className="w-full flex flex-col items-center justify-center text-center gap-4 px-2 md:px-8">
+      <header className="w-full flex flex-col items-center justify-center text-center gap-4 px-2 md:px-12">
         <h2 className="font-extrabold ">
           Anim aute id magna aliqua{" "}
           <span className="text-OrangeRed">ad ad non sunt.</span>
@@ -36,14 +36,34 @@ const Catalogo = ({ products }) => {
 
         <Filtered setDataFiltered={setDataFiltered} />
       </header>
-      <section className="w-full flex flex-col items-center justify-center my-8">
-      {resFilter.length > 0 ? <>
-      </> : <article>
-
-      </article>
-      
-    }
+      <section className="w-full flex flex-col items-center justify-center my-8 px-2 md:px-12">
+        {resFilter.length > 0 ? (
+          <>
+            <article className="w-full pb-8 grid grid-cols-1 place-items-center gap-8 overflow-x-hidden sm:grid-cols-2 sm:px-2 lg:grid-cols-3">
+              {resFilter.map((data) => (
+                <Card key={data.id} data={data} />
+              ))}
+            </article>
+          </>
+        ) : (
+          <article className="w-full pb-8 grid grid-cols-1 place-items-center gap-8 overflow-x-hidden sm:grid-cols-2 sm:px-2 lg:grid-cols-3">
+            {dataCatalogue.map((data) => (
+              <Card key={data.id} data={data} />
+            ))}
+          </article>
+        )}
+        <article className="w-full flex items-center justify-end">
+          <div className="w-auto m-0 border border-DarkGray rounded-md">
+            <button className="px-2 border-r border-DarkGray text-xl font-semibold ">
+              &lt;
+            </button>
+            <button className="px-2 border-l border-DarkGray text-xl font-semibold ">
+              &gt;
+            </button>
+          </div>
+        </article>
       </section>
+
       <ContentContact />
     </Layout>
   );
