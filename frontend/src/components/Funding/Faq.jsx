39a472245d18024d9fe755fd851d22add5d11f66
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-18 22:59:04 -0400

    feat: funding

---
+++ frontend/src/components/Funding/Faq.jsx
@@ -0,0 +1,161 @@
+export const Faq = () => {
+    return (
+        <section class="bg-GhostWhite w-full py-16 px-6">
+            <div class="flex flex-col items-center">
+                <h2 class="font-bold text-4xl mt-5 tracking-tight">Preguntas frecuentes</h2>
+            </div>
+            <div class="grid divide-y divide-neutral-200 max-w-xl mx-auto mt-8">
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+                <div class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span>Mauris ullamcorper imperdiet nec egestas mi quis quam ante?</span>
+                            <span class="transition group-open:rotate-180 text-DarkGray">
+                                <svg
+                                    fill="none"
+                                    height="24"
+                                    shape-rendering="geometricPrecision"
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    viewBox="0 0 24 24"
+                                    width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                        </summary>
+                        <p class="text-neutral-600 mt-3 group-open:animate-fadeIn">
+                            I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quas cupiditate laboriosam fugiat.
+                        </p>
+                    </details>
+                </div>
+            </div>
+        </section>
+    );
+};
---
+++ frontend/src/pages/funding/index.js
@@ -0,0 +1,18 @@
+import { Faq } from '@/components/Funding/Faq';
+import { Header } from '@/components/Funding/Header';
+import { Transactions } from '@/components/Funding/Transactions';
+import { WorkWithUs } from '@/components/Funding/WorkWithUs';
+import Layout from '@/components/Layout';
+
+export default function Funding() {
+    return (
+        <Layout>
+            <Header />
+            <main className="px-[10%] py-24">
+                <Transactions />
+            </main>
+            <WorkWithUs />
+            <Faq />
+        </Layout>
+    );
+}
