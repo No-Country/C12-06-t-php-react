a09958848ea12f48692227315777457f6c6e8602
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-18 21:19:27 -0400

    fix: responsive

--- frontend/src/components/About/AboutHeader.jsx
+++ frontend/src/components/About/AboutHeader.jsx
@@ -2,7 +2,7 @@ export const AboutHeader = () => {
     return (
         <div className="flex items-start overflow-hidden sm:h-[60vh] sm:flex-row flex-col">
             <div className="bg-OrangeRed text-[3.5rem] sm:w-1/2 h-full flex justify-center">
-                <div className="w-[75%] sm:w-[60%] py-8 flex flex-col justify-center">
+                <div className="w-[75%] sm:w-[60%] py-16 flex flex-col justify-center gap-4">
                     <h1 className="text-White font-extrabold tracking-tight leading-none md:text-6xl text-4xl">
                         Conoce más de Nosotros
                     </h1>
