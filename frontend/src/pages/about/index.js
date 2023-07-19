59e72bad3ae44b067a20ab3eccdaacd5d2fa0db0
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-19 18:08:30 -0400

    fix: head tittle

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -1,22 +1,16 @@
-import NavBar from '@/components/NavBar/NavBar';
-import Footer from '@/components/shared/footer/Footer';
-import ContentContact from '@/components/contact/ContentContact';
 import { AboutHeader } from '@/components/About/AboutHeader';
 import { Story } from '@/components/About/Story';
 import { Team } from '@/components/About/Team';
+import Layout from '@/components/Layout';
 
 export default function About() {
     return (
-        <>
-            <NavBar />
+        <Layout page={'Nosotros'}>
             <AboutHeader />
-            <main className="px-[10%] py-24">
+            <section className="px-[10%] py-24">
                 <Story />
                 <Team />
-            </main>
-
-            <ContentContact />
-            <Footer />
-        </>
+            </section>
+        </Layout>
     );
 }
