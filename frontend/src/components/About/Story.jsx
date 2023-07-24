26bee6f86d90fe187faaabe73d52243203f2b64b
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-07-24 18:43:18 -0400

    update: about page

--- frontend/src/components/About/Story.jsx
+++ frontend/src/components/About/Story.jsx
@@ -3,40 +3,45 @@ export const Story = () => {
         <section>
             <h2 className="text-OrangeRed uppercase font-semibold">Historia</h2>
             <div className="flex flex-col gap-8">
-                <h3 className="font-bold text-4xl">Como comienza nuestro camino</h3>
+                <h3 className="font-bold text-4xl">Nuestra Misión: Tu Satisfacción</h3>
                 <div className="flex text-SlateGray text-lg gap-6 sm:flex-row flex-col">
-                    <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-6 md:w-1/2">
                         <p>
-                            Ultrices ultricies a in odio consequat egestas rutrum. Ut vitae aliquam in ipsum. Duis nullam placerat
-                            cursus risus ultrices nisi, vitae tellus in. Qui non fugiat aut minus aut rerum. Perspiciatis iusto
-                            mollitia iste minima soluta id.
+                            En AutoApp, nuestra misión es hacer realidad el sueño de conducir el auto perfecto para cada cliente.
+                            Nos esforzamos por ofrecerte un servicio confiable, transparente y personalizado que supere tus
+                            expectativas.
+                        </p>
+                        <p>Cada vehículo que vendemos cuenta con la garantía de satisfacción y la confianza de nuestra marca.</p>
+                        <h4 className="text-black font-semibold text-xl">¿Qué nos hace diferentes?</h4>
+                        <p>
+                            <span className="font-bold">Experiencia y Trayectoria: </span>
+                            Con años de experiencia en el rubro, hemos perfeccionado nuestro enfoque para ofrecerte una
+                            experiencia de compra y venta de autos usados excepcional.
                         </p>
                         <p>
-                            Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                            eget. Est augue maecenas risus nulla ultrices congue nunc tortor. Eu leo risus porta integer
-                            suspendisse sed sit ligula elit.
-                        </p>
-                        <p>
-                            Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                            eget. Est augue maecenas risus nulla ultrices congue nunc tortor. Eu leo risus porta integer
-                            suspendisse sed sit ligula elit.
+                            Nuestro equipo de expertos está altamente capacitado para brindarte asesoramiento personalizado y
+                            garantizar que encuentres el auto perfecto que se adapte a tus necesidades y presupuesto.
                         </p>
                     </div>
-                    <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-6  md:w-1/2">
                         <p>
-                            Ultrices ultricies a in odio consequat egestas rutrum. Ut vitae aliquam in ipsum. Duis nullam placerat
-                            cursus risus ultrices nisi, vitae tellus in. Qui non fugiat aut minus aut rerum. Perspiciatis iusto
-                            mollitia iste minima soluta id.
+                            <span className="font-bold">Calidad Garantizada: </span>
+                            En AutoApp, la calidad es nuestra prioridad. Todos nuestros autos usados pasan por un riguroso proceso
+                            de inspección y certificación para asegurar que cada vehículo esté en óptimas condiciones. Nos
+                            enorgullecemos de ofrecer autos de calidad y transparencia en cada transacción.
                         </p>
                         <p>
-                            Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                            eget. Est augue maecenas risus nulla ultrices congue nunc tortor. Eu leo risus porta integer
-                            suspendisse sed sit ligula elit.
+                            <span className="font-bold">Concesionario Virtual, Experiencia Real: </span>
+                            Aprovechando la tecnología y el poder del internet, hemos creado una plataforma de concesionario
+                            virtual que te permite explorar nuestra amplia selección de autos usados desde la comodidad de tu
+                            hogar. Navega por nuestro sitio web intuitivo, accede a informes de inspección, realiza consultas y
+                            obtén asesoramiento profesional sin la necesidad de desplazarte.
                         </p>
                         <p>
-                            Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                            eget. Est augue maecenas risus nulla ultrices congue nunc tortor. Eu leo risus porta integer
-                            suspendisse sed sit ligula elit.
+                            <span className="font-bold">Servicio al Cliente Dedicado: </span>
+                            La satisfacción de nuestros clientes es nuestra motivación. Nuestro equipo de servicio al cliente está
+                            aquí para responder tus preguntas, resolver tus inquietudes y brindarte asistencia en cada etapa del
+                            proceso. Queremos que te sientas seguro y confiado al adquirir tu auto usado con nosotros.
                         </p>
                     </div>
                 </div>
